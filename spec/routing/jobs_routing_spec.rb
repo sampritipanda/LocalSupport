6effe02c1f25c9b44112de7bcc7e0400c3432057
David Corking <dmc-source@dcorking.com>
2014-04-09 22:47:59 +0100

    Added jobs routing spec

--- spec/routing/jobs_routing_spec.rb
+++ spec/routing/jobs_routing_spec.rb
@@ -15,29 +15,70 @@ describe JobsController do
                                                   )
     end
 
-    # it "routes to #new" do
-    #   get("/jobs/new").should route_to("jobs#new")
-    # end
-    #
-    # it "routes to #show" do
-    #   get("/jobs/1").should route_to("jobs#show", :id => "1")
-    # end
-    #
-    # it "routes to #edit" do
-    #   get("/jobs/1/edit").should route_to("jobs#edit", :id => "1")
-    # end
-    #
-    # it "routes to #create" do
-    #   post("/jobs").should route_to("jobs#create")
-    # end
-    #
-    # it "routes to #update" do
-    #   put("/jobs/1").should route_to("jobs#update", :id => "1")
-    # end
-    #
-    # it "routes to #destroy" do
-    #   delete("/jobs/1").should route_to("jobs#destroy", :id => "1")
-    # end
+    it "routes to #new" do
+      get("/organizations/#{org.id}/jobs/new").should route_to(
+                                                      {
+                                                          action: 'new',
+                                                          controller: 'jobs',
+                                                          organization_id: '1'
+                                                      }
+                                                               )
+    end
+    
+    it "routes to #show" do
+      get("/organizations/#{org.id}/jobs/9").should route_to(
+                                                      {
+                                                          action: 'show',
+                                                          controller: 'jobs',
+                                                          organization_id: '1',
+                                                          id: '9'
+                                                      }
+                                                               )
+    end
 
+    it "routes to #edit" do
+      get("/organizations/#{org.id}/jobs/9/edit").should route_to(
+                                                      {
+                                                          action: 'edit',
+                                                          controller: 'jobs',
+                                                          organization_id: '1',
+                                                          id: '9'
+                                                      }
+                                                               )
+    end
+
+    it "routes to #create" do
+      post("/organizations/#{org.id}/jobs").should route_to(
+                                                      {
+                                                          action: 'create',
+                                                          controller: 'jobs',
+                                                          organization_id: '1',
+                                                      }
+                                                               )
+    end
+
+
+    it "routes to #update" do
+      put("/organizations/#{org.id}/jobs/9").should route_to(
+                                                      {
+                                                          action: 'update',
+                                                          controller: 'jobs',
+                                                          organization_id: '1',
+                                                          id: '9'
+                                                      }
+                                                               )
+    end
+    
+
+    it "routes to #destroy" do
+      delete("/organizations/#{org.id}/jobs/9").should route_to(
+                                                      {
+                                                          action: 'destroy',
+                                                          controller: 'jobs',
+                                                          organization_id: '1',
+                                                          id: '9'
+                                                      }
+                                                               )
+    end
   end
 end
